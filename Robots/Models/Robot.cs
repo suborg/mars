0d12816bda4d25b17ff009822ccaafa6fe64d7f0
Andrey Borisov <suborg@icloud.com>
2026-04-17 21:11:13 +0300

    movement mechanics relocated to Robot from World

--- Robots/Models/Robot.cs
+++ Robots/Models/Robot.cs
@@ -7,4 +7,73 @@ public class Robot
     public Orientation Orientation { get; set; }
     public CommandsList Commands { get; set; } = new CommandsList();
     public bool IsLost { get; set; } = false;
+
+    public void Execute(Command command, Surface surface)
+    {
+        switch (command)
+        {
+            case Command.L:
+            case Command.R:
+                Rotate(command);
+                break;
+            case Command.F:
+                Move(surface);
+                break;
+        }
+    }
+
+    private void Rotate(Command command)
+    {
+        Orientation = command switch
+        {
+            Command.L => Orientation switch
+            {
+                Orientation.N => Orientation.W,
+                Orientation.E => Orientation.N,
+                Orientation.S => Orientation.E,
+                Orientation.W => Orientation.S,
+                _ => throw new InvalidOperationException("Unsupported orientation")
+            },
+            Command.R => Orientation switch
+            {
+                Orientation.N => Orientation.E,
+                Orientation.E => Orientation.S,
+                Orientation.S => Orientation.W,
+                Orientation.W => Orientation.N,
+                _ => throw new InvalidOperationException("Unsupported orientation")
+            },
+            _ => throw new InvalidOperationException("Unsupported command for rotation")
+        };
+    }
+
+    private void Move(Surface surface)
+    {
+        int nextX = X;
+        int nextY = Y;
+
+        switch (Orientation)
+        {
+            case Orientation.N: nextY += 1; break;
+            case Orientation.E: nextX += 1; break;
+            case Orientation.S: nextY -= 1; break;
+            case Orientation.W: nextX -= 1; break;
+            default: throw new InvalidOperationException("Unsupported orientation");
+        }
+
+        bool outOfBounds = nextX < 0 || nextX > surface.Width || nextY < 0 || nextY > surface.Height;
+
+        if (outOfBounds)
+        {
+            if (!surface.DangerousCells.Contains((X, Y)))
+            {
+                surface.DangerousCells.Add((X, Y));
+                IsLost = true;
+            }
+        }
+        else
+        {
+            X = nextX;
+            Y = nextY;
+        }
+    }
 }
--- Robots/Services/Simulator.cs
+++ Robots/Services/Simulator.cs
@@ -30,7 +30,7 @@ public class Simulator
             {
                 if (robot.IsLost) break;
                 Log($"Executing command: {command}");
-                Execute(robot, command);
+                robot.Execute(command, world.Surface);
                 Log(FormatLog.ASCII(world));
             }
             Log($"Finished commands for robot at ({robot.X}, {robot.Y}) facing {robot.Orientation}. Lost status: {robot.IsLost}");
@@ -38,79 +38,5 @@ public class Simulator
         Log("Simulation completed.");
 
         Log(FormatLog.Format(world));
-    }
-
-    static void Rotate(Robot robot, Command command)
-    {
-        robot.Orientation = command switch
-        {
-            Command.L => robot.Orientation switch
-            {
-                Orientation.N => Orientation.W,
-                Orientation.E => Orientation.N,
-                Orientation.S => Orientation.E,
-                Orientation.W => Orientation.S,
-                _ => throw new InvalidOperationException("Unsupported orientation")
-            },
-            Command.R => robot.Orientation switch
-            {
-                Orientation.N => Orientation.E,
-                Orientation.E => Orientation.S,
-                Orientation.S => Orientation.W,
-                Orientation.W => Orientation.N,
-                _ => throw new InvalidOperationException("Unsupported orientation")
-            },
-            _ => throw new InvalidOperationException("Unsupported command for rotation")
-        };
-    }
-
-    private void Move(Robot robot)
-    {
-        int nextX = robot.X;
-        int nextY = robot.Y;
-
-        switch (robot.Orientation)
-        {
-            case Orientation.N: nextY += 1; break;
-            case Orientation.E: nextX += 1; break;
-            case Orientation.S: nextY -= 1; break;
-            case Orientation.W: nextX -= 1; break;
-            default: throw new InvalidOperationException("Unsupported orientation");
-        }
-
-        if (IsOutOfBounds(nextX, nextY))
-        {
-            if (!world.Surface.DangerousCells.Contains((robot.X, robot.Y)))
-            {
-                world.Surface.DangerousCells.Add((robot.X, robot.Y));
-                robot.IsLost = true;
-            }
-        }
-        else
-        {
-            robot.X = nextX;
-            robot.Y = nextY;
-        }
-    }
-
-    private bool IsOutOfBounds(int x, int y)
-    {
-        return x < 0 || x > world.Surface.Width || y < 0 || y > world.Surface.Height;
-    }
-
-    private void Execute(Robot robot, Command command)
-    {
-        Log($"Executing command: {command} for robot at ({robot.X}, {robot.Y}) facing {robot.Orientation}");
-
-        switch (command)
-        {
-            case Command.L:
-            case Command.R:
-                Rotate(robot, command);
-                break;
-            case Command.F:
-                Move(robot);
-                break;
-        }
     }
 }
